4db41425d588bd9018524f396529b33b8977a520
Paolo <paolor82@gmail.com>
2019-10-09 12:04:53 +0200

    test on entire validation

--- tensorflow2_classification/test_classification.py
+++ tensorflow2_classification/test_classification.py
@@ -34,7 +34,8 @@ def show_batch(image_batch, label_batch):
     plt.show()
 
 data_path = '/data/prota/coco_12_realsize'
-# dir_list = glob.glob(os.path.join(data_path, '*.png'))
+data_path_validation = data_path + '_val'
+
 CLASS_NAMES = np.array([item for item in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, item)) is True])
 
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
@@ -45,12 +46,8 @@ IMG_WIDTH   = 224
 N_EPOCHS    = 50
 
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-train_data_gen = image_generator.flow_from_directory(directory=str(data_path),
-                                                     batch_size=BATCH_SIZE,
-                                                     shuffle=True,
-                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                     classes=list(CLASS_NAMES))
-
+train_data_gen = image_generator.flow_from_directory(directory=str(data_path), batch_size=BATCH_SIZE, shuffle=True, target_size=(IMG_HEIGHT, IMG_WIDTH), classes=list(CLASS_NAMES))
+test_data_gen = image_generator.flow_from_directory(directory=str(data_path_validation), batch_size=BATCH_SIZE, shuffle=False, target_size=(IMG_HEIGHT, IMG_WIDTH), classes=list(CLASS_NAMES))
 
 IMG_SHAPE = (IMG_HEIGHT, IMG_WIDTH, 3)
 
@@ -74,23 +71,28 @@ def train_step(images, labels, step):
     return predictions, loss
 
 @tf.function
-def test_step(images, labels, epoch):
+def test_step(images, labels):
     predictions = model(images)
     t_loss = loss_object(labels, predictions)
     eq = tf.equal(tf.argmax(labels, -1), tf.argmax(predictions, -1))
     accuracy = tf.reduce_mean(tf.cast(eq, tf.float32))
-    with writer.as_default():
-        tf.summary.scalar("test_accuracy", accuracy, step=epoch)
-    return predictions, t_loss
+    return predictions, t_loss, accuracy
 
 epoch = 0
 step = 1
 for batch, label in train_data_gen:
     if train_data_gen.batch_index == 1:
-        test_preds, test_loss = test_step(batch, label, tf.convert_to_tensor(epoch, tf.int64))
-        eq = tf.equal(tf.argmax(label, -1), tf.argmax(test_preds, -1)).numpy()
-        accuracy = np.sum(eq)/len(eq)
-        print('Test Accuracy at epoch [{0:03}]: {1:0.04}'.format(epoch, accuracy * 100))
+        print('test for epoch {}'.format(epoch))
+        test_accuracy_batch_list = []
+        for test_batch, test_label in test_data_gen:
+            if test_data_gen.batch_index == 0:
+                break
+            predictions, t_loss, batch_accuracy = test_step(test_batch, test_label)
+            test_accuracy_batch_list.append(batch_accuracy.numpy())
+        test_accuracy = np.asarray(test_accuracy_batch_list).mean()
+        with writer.as_default():
+            tf.summary.scalar("training_accuracy", test_accuracy * 100, step=epoch)
+        print('Test Accuracy at epoch [{0:03}]: {1:0.04}'.format(epoch, test_accuracy * 100))
 
         epoch += 1
         if epoch == N_EPOCHS:
@@ -100,6 +102,4 @@ for batch, label in train_data_gen:
     train_preds, train_loss = train_step(batch, label, tf.convert_to_tensor(step, tf.int64))
     print('[{0:03}] loss: {1:0.05}'.format(train_data_gen.batch_index, train_loss.numpy()))
     step += 1
-    # print('{0:03} -> {1} {2} {3}'.format(train_data_gen.batch_index, batch[0, 100, 100, 2], batch[1, 120, 100, 1], batch[2, 80, 80, 0]))
-    # show_batch(batch, label)
 
